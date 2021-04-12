d3840fe53a655e92f456c0a04eec35b233404f3b
Talip Can Korkmazer <t.korkmazer@emakina.com.tr>
2021-04-12 21:39:17 +0300

    New h1 tag baba

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
 		<div className="App">
 			<header className="App-header">
 				<img src={logo} className="App-logo" alt="logo" />
-				<p>Talip Reyiz</p>
+				<p>Talip Reyiz baba</p>
 				<a
 					className="App-link"
 					href="https://reactjs.org"
